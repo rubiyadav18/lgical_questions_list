4ee030ee309d34ac29e2d5ae122cc498b72501f2
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-11-23 07:43:55 -0800

    Add files via upload

---
+++ copy_list.py
@@ -0,0 +1,6 @@
+original_list=[10, 34, 56, 23]
+new_list=list(original_list)
+# i took new list vecasue i need copy list 
+print(original_list)
+# i want two things one i want copy list and i want to second new list 
+print(new_list)
