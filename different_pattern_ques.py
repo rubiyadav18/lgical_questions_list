5970f1ca6c3d725067d5364b7190aea533465afa
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-12-28 11:23:31 +0530

    Add files via upload

---
+++ different_pattern_ques.py
@@ -0,0 +1,14 @@
+a=0
+i=1
+while i<=5:
+    a=a+1
+    j=a
+    while j<i+i:
+        print(j,end=" ")
+        j=j+1
+    k=i+i-2
+    while k>a-1:
+        print(k,end=" ")
+        k=k-1
+    print()
+    i=i+1
